cb5bcd246fff20456781eebd2a216dce7776f090
omegarz <bayeemmer@gmail.com>
2018-06-13 16:00:14 +0700

    first change

--- script.js
+++ script.js
@@ -1,57 +1,72 @@
-// User choice
-var userChoice = prompt("Do you choose rock, paper or scissors?");
-if (! userChoice) {
-    // User choice was undefined
-    document.write("<p>Player 1, you cheated! Refresh this screen and fight like a man.</p>");
-} else {
-    // Display user choice
-    document.write("<p>Player 1:" + " " + userChoice + "</p>");
+// welcome message
+console.log("Welcome to Rock-Paper-Scissors Club");
+console.log("When you're ready please type 'new game' to begin your match: Best of 5");
+
+// random generator for computers throw
+function computerPlay() {
+ const ranNo = Math.floor(Math.random() * 3);
+ const choices = ['rock', 'paper', 'scissors']
+ return choices[ranNo];
 }
-// Computer choice
-var computerChoice = Math.random();
-if (computerChoice < 0.34) {
-    computerChoice = "rock";
-} else if(computerChoice <= 0.67) {
-    computerChoice = "paper";
-} else {
-    computerChoice = "scissors";
+
+// prompt user for throw choice
+function promptInput(msg) {
+ let choice = String(window.prompt(msg)).toLowerCase();
+ if(choice == 'scissors' || choice == 'paper' || choice == 'rock') {
+     return choice;
+ } else {
+     return promptInput("That's not a legal throw, try again");
+ }
 }
-// Display computer choice
-document.write("<p>Computer:" + " " + computerChoice + "</p>");
-// Compare user choice vs computer choice
-var compare = function(choice1,choice2) {
-    if (choice1 === choice2) {
-        return "It's a tie!";
-    }
-    if (choice1 === "rock") {
-        if (choice2 === "scissors") {
-            // rock wins
-            return "You win!";
-        } else {
-            // paper wins
-            return "You lose! Try again.";
-        }
-    }
-    if (choice1 === "paper") {
-        if (choice2 === "rock") {
-            // paper wins
-            return "You win!";
-        } else {
-            // scissors wins
-            return "You lose! Try again.";
-        }
-    }
-    if (choice1 === "scissors") {
-        if (choice2 === "rock") {
-            // rock wins
-            return "You lose! Try again.";
-        } else {
-            // scissors wins
-            return "You win!";
-        }
-    }
+
+// single round code
+function playRound(playerSelection, computerSelection) {
+ if (playerSelection === computerSelection) {
+     return 'draw';
+ } else if (
+     playerSelection === 'rock' && computerSelection == 'scissors'
+ ||  playerSelection === 'paper' && computerSelection == "rock"
+ ||  playerSelection === 'scissors' && computerSelection == 'paper') {
+     return 'win';
+ } else {
+     return 'lose'
+ }
+}
+
+// main game best of 5 loop
+function game() {
+
+ let wins = 0;
+ let losses = 0;
+
+ for(let i = 1; i < 6; i++) {
+     console.log("=========");
+     console.log("ROUND " + i);
+     const choice = String(promptInput("Please choose rock, paper or scissors")).toLowerCase();
+     const computerChoice = computerPlay();
+
+     switch(playRound(choice, computerChoice)) {
+         case 'win':
+             console.log(choice + " beats " + computerChoice);
+             wins++;
+             break;
+         case 'lose':
+             console.log(choice + " loses to " + computerChoice);
+             losses++;
+             break;
+         case 'draw':
+             console.log("Close one, two " + choice +"s makes it a draw!");
+             break;
+     }
+ }
+
+ console.log("=========");
+ if(wins > losses) {
+     console.log('CONGRATULATIONS! YOU WIN');
+ } else if (losses > wins) {
+     console.log('Nice try but the AI beat you, better luck next time!')
+ } else {
+     console.log('It was a draw, better luck next time!')
+ }
+ console.log("you won " + wins + " rounds out of 5");
 };
-// Run the compare function
-var results = compare(userChoice,computerChoice);
-// Display results
-document.write("<br><hr><br>" + results);
